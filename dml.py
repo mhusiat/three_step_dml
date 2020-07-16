92bc18b357787f474defb7e86cbdf4e985e856e3
Maciej Husiatyński <maciej.husiatynski@gmail.com>
2020-07-16 13:54:10 +0200

    initial version of the project

---
+++ dml.py
@@ -0,0 +1,398 @@
+from multiprocessing import Pool
+
+import numpy as np
+import pandas as pd
+
+
+def _two_step_orthogonalization(
+    nfolds: int,
+    tsize: int,
+    df_folds: list,
+    fold_combinations: tuple,
+    nuisance_estim: list,
+    ensemble_weights: np.array,
+) -> tuple:
+    """
+    orthogonalize features with an ensemble of estimators using precomputed
+    set of ensemble weights (following Chernozhukov et al., 2017)
+    """
+    # initiate the list storage for orthogonalized features
+    orthogonalized_target_and_treatment = []
+
+    for cbn in fold_combinations:
+        # determine what folds have what task in the current run of estimation
+        linear_folds = cbn[: nfolds[0]]
+        nuisance_folds = cbn[nfolds[0] :]
+
+        # split samples into 2 parts: training the nuisance parameters and
+        # estimating the parameters of interest on orthogonalized features
+        df_train = np.vstack([df_folds[c] for c in nuisance_folds])
+        df_params = np.vstack([df_folds[c] for c in linear_folds])
+
+        # initialize fitted values of treatment regressors
+        fitted_values = np.zeros([df_params.shape[0], tsize + 1, len(nuisance_estim)])
+        estimators_linear = np.zeros([df_params.shape[0], tsize + 1])
+
+        # fit each variable of interest seperately against the nuisance params
+        # note that there are tsize treatment features + 1 target feature
+        for t in range(tsize + 1):
+            for which, estim in enumerate(nuisance_estim):
+                # train the model using nuisance sample
+                estim.fit(df_train[:, tsize + 1 :], df_train[:, t])
+
+                # fit values using the linear sample
+                fitted_values[:, t, which] = estim.predict(df_params[:, tsize + 1 :])
+
+            # use pre-computed weights to combine the nuisance estimators
+            estimators_linear[:, t] = fitted_values[:, t, :].dot(ensemble_weights[:, t])
+
+        # initialize orthogonalized features for each ensemble estimator
+        orthogonal_features = df_params[:, : tsize + 1] - estimators_linear
+        orthogonalized_target_and_treatment.append(orthogonal_features)
+
+    # return stacked orthogonalized features; note that order
+    # of observations needs to be preserved here
+    return np.vstack(orthogonalized_target_and_treatment)
+
+
+def _three_step_orthogonalization(
+    nfolds: int,
+    tsize: int,
+    df_folds: list,
+    fold_combinations: tuple,
+    nuisance_estim: list,
+    ensemble_estim: list,
+) -> tuple:
+    """
+    orthogonalize features with an average over ensembles of estimators which
+    are computed using an extra fold (hence 3 steps); this is a similar
+    procedure as DML in Chernozhukov et al. (2017) but with an extra step
+    in the middle instead of cross-validation prior to estimation
+    """
+    # initiate the list storage for orthogonalized features
+    orthogonalized_target_and_treatment = []
+
+    # routine is rerun nfold times so that each fold is used
+    # in different tasks the same amount of times
+    for cbn in fold_combinations:
+
+        # determine what folds have what task in the current run of estimation
+        linear_folds = cbn[: nfolds[0]]
+        ensemble_folds = cbn[nfolds[0] : nfolds[0] + nfolds[1]]
+        nuisance_folds = cbn[nfolds[0] + nfolds[1] :]
+
+        # split samples into 3 parts: training the nuisance parameters;
+        # determining ensemble weights; estimating the parameters of interest
+        df_train = np.vstack([df_folds[c] for c in nuisance_folds])
+        df_ensemble = np.vstack([df_folds[c] for c in ensemble_folds])
+        df_params = np.vstack([df_folds[c] for c in linear_folds])
+
+        # initialize fitted values for target and treatment features
+        estimators_ensemble = np.zeros(
+            [df_ensemble.shape[0], tsize + 1, len(nuisance_estim)]
+        )
+        estimators_linear_nuisance = np.zeros(
+            [df_params.shape[0], tsize + 1, len(nuisance_estim)]
+        )
+        estimators_linear_ensemble = np.zeros(
+            [df_params.shape[0], tsize + 1, len(ensemble_estim)]
+        )
+
+        # fit each variable of interest seperately against the nuisance params
+        # and predict orthogonalized features using ensemble and linear samples
+        for i in range(tsize + 1):
+            for which, estim in enumerate(nuisance_estim):
+                # train the model using the train sample only
+                estim.fit(df_train[:, tsize + 1 :], df_train[:, i])
+
+                # predict on both ensemble and linear params samples
+                estimators_ensemble[:, i, which] = estim.predict(
+                    df_ensemble[:, tsize + 1 :]
+                )
+                estimators_linear_nuisance[:, i, which] = estim.predict(
+                    df_params[:, tsize + 1 :]
+                )
+
+            for which, estim in enumerate(ensemble_estim):
+                # train ensemble using fitted values from previous step
+                estim.fit(estimators_ensemble[:, i, :], df_ensemble[:, i])
+
+                # and predict the features using fitted values on linear
+                # parameters sample and trained weights on ensemble sample
+                estimators_linear_ensemble[:, i, which] = estim.predict(
+                    estimators_linear_nuisance[:, i, :]
+                )
+        # average over the predictions of different ensemble methods used
+        averaged_ensembles = np.mean(estimators_linear_ensemble, axis=2)
+
+        # orthonalize the target and linear features against fitted values
+        orthogonal_features = df_params[:, : tsize + 1] - averaged_ensembles
+
+        # note that order of linear folds needs to be preserved here
+        orthogonalized_target_and_treatment.append(orthogonal_features)
+
+    # combine list of orthogonalized features into a single array
+    return np.vstack(orthogonalized_target_and_treatment)
+
+
+def _run_double_machine_learning(
+    df: np.array,
+    tsize: int,
+    nuisance_estim: list,
+    ensemble_estim: list,
+    ensemble_weights: np.array,
+    nfolds,
+):
+    """
+    wrapper function that fits a single cross-fitting run of the model
+    used by the .fit() method on the DoubleMachineLearner class object
+    """
+    # create sum(nfolds) combinations of folds so that each piece of data is
+    # used the same amount of times in each part throughout the estimation
+    fold_combinations = [
+        list(range(i, sum(nfolds))) + list(range(0, i)) for i in range(sum(nfolds))
+    ]
+
+    # determine fold size and fold the dataset (approximately) evenly
+    fold_size = int(np.floor(df.shape[0] / sum(nfolds)))
+    df_folds = np.split(df, [fold_size * which for which in range(1, sum(nfolds))])
+
+    if len(nfolds) == 2:
+        orthogonalized_features = _two_step_orthogonalization(
+            nfolds, tsize, df_folds, fold_combinations, nuisance_estim, ensemble_weights
+        )
+    elif len(nfolds) == 3:
+        orthogonalized_features = _three_step_orthogonalization(
+            nfolds, tsize, df_folds, fold_combinations, nuisance_estim, ensemble_estim
+        )
+    else:
+        raise ValueError("there should be either 2 or 3 sets of folds")
+
+    # split the results into target and treatment features
+    Y = orthogonalized_features[:, 0]
+    T = orthogonalized_features[:, 1:]
+
+    DML_estimates = np.linalg.inv(np.dot(T.T, T)).dot(np.dot(T.T, Y))
+
+    # note that variance estimates still need a finite sample correction
+    residuals = Y - T.dot(DML_estimates)
+    asymptotic_variance_estimates = np.mean(residuals ** 2) / T.T.dot(T)
+
+    return DML_estimates, np.diag(asymptotic_variance_estimates)
+
+
+def ensemble_weights_cv(
+    X: np.array,
+    y: np.array,
+    nuisance_estimators: list,
+    ensemble_estimator: object,
+    nfolds=5,
+) -> np.array:
+    """
+    helper function to pre-estimate ensemble weights for k features in
+    Double Machine Learning algorithm using nfolds cross-validation
+
+    takes:
+        X: n x m numpy array of nuisance features used for training
+
+        y: n x k numpy array of k features that are estimated
+
+        nuisance_estimators: list of sklearn-like estimator objects
+                             used to estimate the k features of interest
+
+        ensemble_estimator: sklearn-like estimator for ensemble weights
+
+        nfolds: number of folds used in the cross-validation routine
+
+    returns:
+        n x k array of weights; where n = nr of estimators used in the ensemble
+    """
+    # stack features together for consistent splitting in cross-validation
+    df = np.hstack([y, X])
+
+    # create sum(nfolds) combinations of folds so that each piece of data is
+    # used the same amount of times throughout the estimation
+    fold_combinations = [
+        list(range(i, nfolds)) + list(range(0, i)) for i in range(nfolds)
+    ]
+
+    # determine fold size and fold the dataset (approximately) evenly
+    sample_fold = int(np.floor(df.shape[0] / nfolds))
+    df_folds = np.split(df, [sample_fold * i for i in range(1, nfolds)])
+
+    # initiate final weights matrix
+    final_weights = np.zeros([len(nuisance_estimators), y.shape[1]])
+
+    for cbn in fold_combinations:
+        # assign roles to folds in the current run
+        ensemble_sample = df_folds[0]
+        train_sample = np.vstack(df_folds[1:])
+
+        # initiate the weights for each ensemble and feature in this run
+        current_run_weights = np.zeros([len(nuisance_estimators), y.shape[1]])
+        for t in range(y.shape[1]):
+            # initiate fitted values array
+            fitted_values = np.zeros(
+                [ensemble_sample.shape[0], len(nuisance_estimators)]
+            )
+
+            for which, estimator in enumerate(nuisance_estimators):
+                # train the nuisance parameter estimator
+                estimator.fit(train_sample[:, y.shape[1] :], train_sample[:, t])
+
+                # fit the values on the ensemble sample
+                fitted_values[:, which] = estimator.predict(
+                    ensemble_sample[:, y.shape[1] :]
+                )
+            # estimate weights of fitted values against ensemble sample target
+            ensemble_estimator.fit(fitted_values, ensemble_sample[:, t])
+
+            # store the weights for the feature t of the current run
+            current_run_weights[:, t] = ensemble_estimator.coefs_
+
+        # update final weights with set of weights for each of the k features
+        # estimated divided by the number of nfold cross-validation runs
+        final_weights += current_run_weights / nfolds
+
+    return final_weights
+
+
+class DoubleMachineLearner:
+    """
+    double machine (DML2) learning as presented by Chernozhukov et al. (2017)
+    paper with additional intermediate step to fit a list of ensemble methods
+    similar to Bajari et al. (2015) paper
+    """
+
+    def __init__(
+        self,
+        nuisance_estimators,
+        ensemble_estimators=None,
+        ensemble_weights=None,
+        crossfit_runs=50,
+        nfolds=(1, 1, 3),
+    ):
+        """
+        initialize the estimator
+
+        takes:
+
+            nuisance_estimators: list of sklearn or sklearn-like estimators
+            used to fit the nuisance paramaters
+
+            ensemble_estimators: list of sklearn or sklearn-like estimators
+            used to fit the ensemble weights across models
+
+            crossfit_runs: number of random splits of datasets and consequent
+            estimation of parameters to average out the bias resulting from a
+            given choice of splitting the data
+
+        the dependance on sklearn-like estimators is necessary since functions
+        often call for .fit() and .predict() methods of the estimators
+        """
+        # ensure that folds are integers for correct splitting of the dataset
+        for _fold_size in nfolds:
+            if type(_fold_size) != int:
+                raise TypeError("fold size should only be expressed in integers")
+
+        # NOTE: temporary fix on linear fold to be exactly of size 1; this may be
+        # changed in future version but for now it decreases computational burden
+        # of the estimator and avoids another layer of prediction averaging
+        if nfolds[0] != 1:
+            raise ValueError("linear param fold should be fixed at 1 for now")
+
+        self.nuisance_estimators = nuisance_estimators
+        self.ensemble_estimators = ensemble_estimators
+        self.crossfit_runs = crossfit_runs
+        self.nfolds = nfolds
+
+    def fit(
+        self, X: np.array, T: np.array, Y: np.array, cores_used=1, ensemble_weights=None
+    ):
+        """
+        fit the Double Machine Learning estimator
+
+        takes:
+
+            Y: dependant variable (target); N x 1 column numpy array
+
+            T: features for which linear parameters are estimates; N x T array
+               (also sometimes referred to as 'treatment' within the code)
+
+            X: features used to estimate nuisance parameters; N x M array
+
+            cores_used: how many multiple processes are used at the same time
+            since crossfit runs are independent of each other, multiprocessing
+            package is used to leverage this and speed up the estimation
+        """
+        # ensure that features are provided in a correct format and size
+        if Y.shape[0] != T.shape[0] or Y.shape[0] != X.shape[0]:
+            raise ValueError("length of observations does not match for inputs")
+
+        if Y.shape[1] != 1:
+            raise ValueError("dependant variable should be a Nx1 column vector")
+
+        if self.ensemble_estimators is None and ensemble_weights is None:
+            raise ValueError(
+                "you need to either provide pre-computed ensemble "
+                "weights or specify a set of estimators for them"
+            )
+
+        # stack features together and convert into a dataframe; this simplifies
+        # consistent multiple splits of the dataset across estimation
+        dataframe = pd.DataFrame(np.hstack([Y, T, X]))
+        treatment_size = T.shape[1]  # record nr of the treatment features
+
+        # generate multiple shuffled index orderings for random data splits
+        # across crossfit runs; this is done to average out the bias due to
+        # making a random split in the data for different parts of the estimator
+        # (done beforehand to avoid identical splits in multiprocessing)
+        shuffled_index = [
+            np.random.choice(dataframe.index, dataframe.shape[0], replace=False)
+            for _ in range(self.crossfit_runs)
+        ]
+
+        # create empty arrays for storing crossfit results across estimators
+        estimates_array = np.zeros([self.crossfit_runs, treatment_size])
+        variance_array = np.zeros([self.crossfit_runs, treatment_size])
+
+        # use multiprocessing for simultaenous model estimation across crossfit
+        # runs; since these are unrelated, asynchronous multiprocessing allows
+        # to speed up the estimation process substantially
+        with Pool(processes=cores_used) as mp_pool:
+            pool_of_tasks = [
+                mp_pool.apply_async(
+                    _run_double_machine_learning,
+                    args=(
+                        dataframe.loc[shuffled_index[i], :],
+                        treatment_size,
+                        self.nuisance_estimators,
+                        self.ensemble_estimators,
+                        ensemble_weights,
+                        self.nfolds,
+                    ),
+                )
+                for i in range(self.crossfit_runs)
+            ]
+
+            parallel_results = [p.get() for p in pool_of_tasks]
+
+        # unpack the results after finishing all parallel crossfit runs
+        for which, results in enumerate(parallel_results):
+            estimates_array[which, :], variance_array[which, :] = results
+
+        # create average estimates across the ensemble estimators
+        self.averaged_estimate = np.mean(estimates_array, axis=0)
+
+        # estimate variance for each estimate; note that this is a
+        # finite-sample mean or median corrected variance that corrects for
+        # random splits within each cross-fit run of the estimator
+        self.mean_corrected_variance = np.mean(
+            variance_array + (estimates_array - np.mean(estimates_array, axis=0)) ** 2,
+            axis=0,
+        )
+        self.median_corrected_variance = np.mean(
+            variance_array
+            + (estimates_array - np.median(estimates_array, axis=0)) ** 2,
+            axis=0,
+        )
---
+++ dml_simulations.py
@@ -0,0 +1,277 @@
+from time import time
+import warnings
+from collections import OrderedDict
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from scipy.optimize import Bounds, LinearConstraint, minimize
+from sklearn.ensemble import GradientBoostingRegressor, RandomForestRegressor
+from sklearn.linear_model import ElasticNetCV
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.svm import SVR
+from sklearn.kernel_ridge import KernelRidge
+
+from dml import DoubleMachineLearner, ensemble_weights_cv
+
+np.random.seed(42)
+
+
+def m(X: np.array) -> np.array:
+    """ helper function to get non-linear relations """
+    f1_ = (X - 2 * X ** 2 - 3 * X ** 3) / 10
+    f2_ = np.sin(X) * 2
+    f2_[(X > 0.5) & (X < 1)] = 2
+    f_ = f1_ + f2_
+    f_[X > 3] = 3
+    f_[(X > 0) & (X < 1)] = 2
+
+    return -np.clip(-f_, a_min=-7, a_max=7)
+
+
+def g(X: np.array) -> np.array:
+    """ helper function to get non-linear relations """
+    f1_ = (X - 3 * X ** 2) / 10
+    f2_ = -np.sin(X)
+    f2_[(X > 0.5) & (X < 2)] = 2
+    f_ = f1_ + f2_
+    f_[(X > 1) & (X < 2)] = -2
+
+    return np.clip(f_, a_min=-7, a_max=7)
+
+
+def plot_mX_gX(path="dml_simulations_data_graph.pdf") -> None:
+    """ a wrapper to plot graphs of non-linear functions for reference """
+    fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(10, 4))
+    ax = axes.flatten()
+
+    x = np.linspace(-5, 5, 1000)
+
+    ax[0].plot(x, g(x))
+    ax[0].set_title("g(x) function on linear feature T")
+    ax[1].plot(x, m(x))
+    ax[1].set_title("m(x) function on target feature Y")
+
+    plt.savefig(path)
+    plt.clf()
+    return None
+
+
+def gen_sim_data(n=1000, k=4, param=[0.5]) -> np.array:
+    """
+    functional form of data-generating process fixed here; for estimating
+    2+ linear parameters this function needs to be adjusted manually
+    """
+    X = np.random.randn(n, k)
+    T = g(X[:, 0]) + np.random.randn(n)
+    Y = param * T + m(X[:, 0]) + np.random.randn(n)
+
+    # return a "target | linear_features | nuisance features" ordered array
+    return np.hstack([Y.reshape(-1, 1), T.reshape(-1, 1), X])
+
+
+def sum_square(b: np.array, X: np.array, y: np.array) -> float:
+    """ compute sum of squared errors for given beta vector """
+    return np.sum((y.reshape(-1, 1) - X.dot(b).reshape(-1, 1)) ** 2)
+
+
+class ExactWeights:
+    def __init__(self):
+        """ sklearn-like object; estimates weights where sum(weights)=1 """
+        self.name = "custom weight estimator"
+
+    def fit(self, X: np.array, Y: np.array):
+        # constraint weights to be between 0 and 1 and sum up to 1
+        bounds = Bounds(0, 1)
+        constraints = LinearConstraint(np.ones(X.shape[1]), 1, 1)
+
+        # ignore particular warning about setting the hessian to zero
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+
+            # set starting values at equal weights for each estimator
+            self.coefs_ = minimize(
+                sum_square,
+                np.ones(X.shape[1]) * 0.5,
+                args=(X, Y),
+                method="trust-constr",
+                constraints=constraints,
+                bounds=bounds,
+            ).x
+
+    def predict(self, Xp):
+        return Xp.dot(self.coefs_)
+
+
+"""
+
+if executed as the main script, will run simulations for 2 variants of the
+ensemble Double Machine Learning algorithm as in Chernozhukov et al. (2017):
+
+- 'classic' ensemble DML2 as in Chernozhukov et al. (2017) with ensemble
+   weights summing uo to 1 pre-computed with 5-fold cross-validation
+
+- 'custom' ensemble DML2 similar to Chernozhukov et al. (2017) but with
+   step in the middle to compute cross-fit-run specific ensemble weights;
+   ensemble weights also sum up to 1 and are restricted to be positive
+   this is similar approach to one taken in Bajari et al. (2015)
+
+note that custom estimator takes an average of the estimates from given
+ensemble methods if more than 1 ensemble method is provided
+
+References:
+   Chernozhukov, V., Chetverikov, D., Demirer, M., Duflo, E., Hansen, C.,
+   Newey, W., and Robins, J. (2017) Double/debiased machine learning for
+   treatment and structural parameters. The Econometrics Journal, 21(1), 1-68.
+
+   Bajari, P., Nekipelov, D., Ryan, S.P., and Yang, M. (2015) Machine learning
+   methods for demand estimation. American Economic Review, 105(5), 481-485.
+"""
+
+# specify tuples of estimators for nuisance and ensemble estimators
+NUISANCE_ESTIMATORS = [
+    ("Elastic Net", ElasticNetCV(cv=3)),
+    ("K nearest neighbors", KNeighborsRegressor(n_neighbors=50, weights="distance")),
+    ("Support Vector Machines", SVR(gamma="auto")),
+    ("Random Forest", RandomForestRegressor(n_estimators=100, max_features="sqrt")),
+    ("Boosted Trees", GradientBoostingRegressor(n_estimators=80)),
+    ("Kernel Ridge", KernelRidge()),
+]
+ENSEMBLE_ESTIMATORS = [ExactWeights()]
+
+SIMULATIONS, CROSSFIT_RUNS, PARAM_VALUE = 200, 50, [0.5]
+CORES_USED = 10  # multiprocessing is used in parallel at crossfiting level
+
+# specify combinations of sample x nr_features for simulations
+# this means that there will be len(N)*len(K)*SIMULATIONS runs
+N = [500, 1000]
+K = [2, 8]
+
+SIMULATION_DML_ESTIMATORS = OrderedDict(
+    [
+        (
+            "classic",
+            DoubleMachineLearner(
+                [estimator for _, estimator in NUISANCE_ESTIMATORS],
+                crossfit_runs=CROSSFIT_RUNS,
+                nfolds=(1, 3),
+            ),
+        ),
+        (
+            "custom",
+            DoubleMachineLearner(
+                [estimator for _, estimator in NUISANCE_ESTIMATORS],
+                ensemble_estimators=[ENSEMBLE_ESTIMATORS[0]],
+                crossfit_runs=CROSSFIT_RUNS,
+                nfolds=(1, 1, 3),
+            ),
+        ),
+    ]
+)
+
+if __name__ == "__main__":
+    # plot the m() and g() functions to have a visual reference
+    # on what kind of non-linear functions are fitted
+    plot_mX_gX()
+
+    # create a table of output that will store results across n-k combinations
+    RESULTS_TABLE_PATH = "dml_simulation_results.txt"
+    multi_index, multi_column = [], []
+    for n in N:
+        multi_index.append((n, "bias"))
+        multi_index.append((n, "acc"))
+    for k in K:
+        (multi_column.append((k, name)) for name in SIMULATION_DML_ESTIMATORS.keys())
+
+    index = pd.MultiIndex.from_tuples(multi_index, names=["size", "measure"])
+    column = pd.MultiIndex.from_tuples(multi_column, names=["features", "estimator"])
+    res_frame = pd.DataFrame({}, index=index, columns=column)
+
+    start = time()
+    for n in N:
+        for k in K:
+
+            # initiate objects to store the results of this n-k combination
+            SIMULATION_RESULTS = {name: [] for name in SIMULATION_DML_ESTIMATORS.keys()}
+            TABLE_BIAS = {name: [] for name in SIMULATION_DML_ESTIMATORS.keys()}
+            TABLE_ACC = {name: 0 for name in SIMULATION_DML_ESTIMATORS.keys()}
+
+            for run in range(SIMULATIONS):
+                data = gen_sim_data(n=n, k=k, param=PARAM_VALUE)
+
+                # precompute weights that will be used across
+                # cross-fitting for the classic method
+                weights = ensemble_weights_cv(
+                    data[:, 1 + len(PARAM_VALUE) :],
+                    data[:, : 1 + len(PARAM_VALUE)],
+                    [estimator for _, estimator in NUISANCE_ESTIMATORS],
+                    ENSEMBLE_ESTIMATORS[0],
+                    nfolds=5,
+                )
+
+                for estimator_name, dml_estimator in SIMULATION_DML_ESTIMATORS.items():
+                    if estimator_name == "classic":
+                        # classic version needs to be provided with weights
+                        dml_estimator.fit(
+                            data[:, 1 + len(PARAM_VALUE) :],
+                            data[:, 1 : 1 + len(PARAM_VALUE)],
+                            data[:, [0]],
+                            cores_used=CORES_USED,
+                            ensemble_weights=weights,
+                        )
+                    else:
+                        # custom version estimates weights with middle step
+                        dml_estimator.fit(
+                            data[:, 1 + len(PARAM_VALUE) :],
+                            data[:, 1 : 1 + len(PARAM_VALUE)],
+                            data[:, [0]],
+                            cores_used=CORES_USED,
+                        )
+
+                    SIMULATION_RESULTS[estimator_name].append(
+                        (
+                            dml_estimator.averaged_estimate,
+                            np.sqrt(dml_estimator.mean_corrected_variance),
+                        )
+                    )
+
+                # this variable only provides the estimated runtime for the
+                # current n-k combination (runtime differs largely by n-k)
+                _avg_runtime = (time() - start) / (run + 1)
+                est_time = round(_avg_runtime * (SIMULATIONS - run + 1) / 3600, 2)
+                print(f"n: {n}; k: {k}; run {run}; remaining: {est_time}h\r", end="")
+
+            for name, results in SIMULATION_RESULTS.items():
+                for estimate, standard_errors in results:
+
+                    # store the bias as average mean squared error
+                    TABLE_BIAS[name].append(PARAM_VALUE - estimate)
+
+                    # also check if the confidence intervals calculated
+                    # capture the true value (should be ~ 95%)
+                    if (
+                        estimate - 1.96 * standard_errors
+                        <= PARAM_VALUE
+                        <= estimate + 1.96 * standard_errors
+                    ):
+                        TABLE_ACC[name] += 1 / SIMULATIONS
+
+            # simulations can take a while so print results as they arrive
+            # note that the results are stored within a dataframe regardless
+            for name, bias in TABLE_BIAS.items():
+                res_frame.loc[(n, "bias"), (k, name)] = np.mean(np.array(bias) ** 2)
+                print(
+                    f"n={n}, k={k}, estimator={name}; bias: "
+                    f"{np.round(np.mean(np.array(bias)**2), 7)}"
+                )
+
+            for name, accuracy in TABLE_ACC.items():
+                res_frame.loc[(n, "acc"), (k, name)] = f"({accuracy})"
+                print(
+                    f"n={n}, k={k}, estimator={name}; "
+                    f"accuracy: {np.round(accuracy, 7)}"
+                )
+
+    # store the results table in the main folder for reference
+    with open(RESULTS_TABLE_PATH, "w") as f:
+        f.write(res_frame.to_latex(multicolumn_format="c"))
